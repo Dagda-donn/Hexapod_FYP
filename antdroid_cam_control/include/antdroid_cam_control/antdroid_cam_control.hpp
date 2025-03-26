f398da19fe4a5e2cb1df93b2b17c4ef1f3269fbf
Dagda-donn <dagdadonn@gmail.com>
2025-03-26 13:18:51 +0000

    Update antdroid_cam_control.hpp

--- antdroid_cam_control/include/antdroid_cam_control/antdroid_cam_control.hpp
+++ antdroid_cam_control/include/antdroid_cam_control/antdroid_cam_control.hpp
@@ -17,61 +17,42 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-
-#ifndef ANTDROID_CAM_CONTROL_HPP_
-#define ANTDROID_CAM_CONTROL_HPP_
-
-#include <ros/ros.h>
-#include <image_transport/image_transport.h>
-#include <cv_bridge/cv_bridge.h>
-#include <sensor_msgs/image_encodings.h>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/core/core.hpp>
-#include <geometry_msgs/Twist.h>
-
-static const std::string GREEN_HSV= "Green HSV Filter";
-static const std::string GREEN_BALL= "Green Ball Detection";
-static const std::string RED_HSV= "Red HSV Filter";
-static const std::string RED_BALL= "Red Ball Detection";
-
-
-
-static const bool RED = 1;
-static const bool GREEN = 0;
-
-static const int NEAR_BALL = 60; // radius of ball when is too near
-
-static const int LEFT_WIDTH_ROTATE = 140;
-static const int RIGHT_WIDTH_ROTATE = 500;
-
-
-using namespace cv;
-
-
+ #ifndef ANTDROID_CAM_CONTROL_HPP_
+ #define ANTDROID_CAM_CONTROL_HPP_
  
-class ImageConverter
-{
-    ros::NodeHandle _nh;
-    image_transport::ImageTransport _it;
-    image_transport::Subscriber _image_sub;
-//    image_transport::Publisher image_pub_;
-    ros::Publisher _vel_pub;
-    bool _block_green;
-    bool _is_test;
-
-    int _count;
-
-    public:
-    ImageConverter();
+ #include <ros/ros.h>
+ #include <image_transport/image_transport.h>
+ #include <cv_bridge/cv_bridge.h>
+ #include <sensor_msgs/image_encodings.h>
+ #include <opencv2/imgproc/imgproc.hpp>
+ #include <opencv2/highgui/highgui.hpp>
+ #include <opencv2/core/core.hpp>
+ #include <geometry_msgs/Twist.h>
  
-    ~ImageConverter();
+ static const int LEFT_WIDTH_ROTATE = 140;
+ static const int RIGHT_WIDTH_ROTATE = 500;
  
-    void imageCb(const sensor_msgs::ImageConstPtr& msg);
-    Mat redFilter(Mat src);
-    Mat greenFilter(Mat src);
-    Mat refineImage(Mat src);
-    Mat trackBall(Mat src, const bool is_red);
-};
-
-#endif
+ using namespace cv;
+ 
+ class ImageConverter
+ {
+     ros::NodeHandle _nh;
+     image_transport::ImageTransport _it;
+     image_transport::Subscriber _image_sub;
+     ros::Publisher _vel_pub;
+     bool _is_test;
+ 
+     int _count;
+ 
+ public:
+     ImageConverter();
+     ~ImageConverter();
+ 
+     void imageCb(const sensor_msgs::ImageConstPtr& msg);
+ 
+     // Light tracking methods
+     void trackLight(Mat src); // Track the brightest area and move towards it
+ };
+ 
+ #endif
+ 
